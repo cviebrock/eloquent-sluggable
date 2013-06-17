4a2e9ac8580daa84f5d940ea4106b350ebbba99f
Colin Viebrock <colin@viebrock.ca>
2013-06-17 12:22:46 -0500

    update type hinting

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,7 @@
 ## 1.0.0-beta2 - xx-xxx-2013
 
 - `$sluggable` property of model switched to static.
+- update type hinting in Sluggable::make()
 
 
 ## 1.0.0-beta - 11-Jun-2013
--- src/Cviebrock/EloquentSluggable/Sluggable.php
+++ src/Cviebrock/EloquentSluggable/Sluggable.php
@@ -2,6 +2,7 @@
 
 
 use Closure;
+use Illuminate\Database\Eloquent\Model;
 
 class Sluggable {
 
@@ -26,11 +27,11 @@ class Sluggable {
 	/**
 	 * Make a slug for the model
 	 *
-	 * @param  \Eloquent $model The model
+	 * @param  Model     $model The model
 	 * @param  boolean   $force Force generation of a slug
 	 * @return boolean
 	 */
-	public function make( \Eloquent $model, $force = false)
+	public function make( Model $model, $force = false)
 	{
 
 		// if the model isn't sluggable, then do nothing
