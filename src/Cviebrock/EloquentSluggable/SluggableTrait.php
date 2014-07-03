53efa5adc2789f0f0f6a959157254647d1660996
Paris Paraskeva <paris@unitedworx.com>
2014-07-03 12:56:19 +0300

    Update SluggableTrait.php
    if on_update_use_slug option is true and the slug has been chnaged then update the slug based on itself instead of the source attribute

--- src/Cviebrock/EloquentSluggable/SluggableTrait.php
+++ src/Cviebrock/EloquentSluggable/SluggableTrait.php
@@ -10,7 +10,11 @@ trait SluggableTrait {
 		$on_update = $this->sluggable['on_update'];
 
 		if ($this->isDirty($save_to)) {
-			return false;
+			if ($this->sluggable['on_update_use_slug']){
+				return true;
+			} else {
+				return false;
+			}
 		}
 
 		return ( !$this->exists || empty($this->{$save_to}) || $on_update );
@@ -202,6 +206,15 @@ trait SluggableTrait {
 		{
 
 			$source = $this->getSlugSource();
+			
+			//if on_update_use_slug is true then use the slug itself as the source
+			if ($this->sluggable['on_update_use_slug']){
+                		$save_to = $this->sluggable['save_to'];
+                		if ( ($this->getAttribute( $save_to )!='') && ($this->isDirty($save_to)) ){         
+                        		$source = $this->getAttribute( $save_to );  
+                		} 
+            		}
+            		
 			$slug = $this->generateSlug($source);
 
 			$slug = $this->validateSlug($slug);
