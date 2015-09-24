7c342e225a70839dd6f0b35307daccc1d9e3075a
Leo Sjöberg <leo.sjoberg@gmail.com>
2015-09-25 00:44:35 +0200

    Added a method to generate a unique slug statically without attaching it to a model

--- tests/SluggableTest.php
+++ tests/SluggableTest.php
@@ -783,4 +783,26 @@ class SluggableTest extends TestCase
         $this->assertEquals($post->subtitle, 'I have been slugged!');
     }
 
+    /**
+     * Test that we can generate a slug statically.
+     * 
+     * @test
+     */
+    public function testStaticSlugGenerator()
+    {
+        $this->assertEquals('my-test-post', Post::createSlug('My Test Post'));
+    }
+
+    /**
+     * Test that we generate unique slugs in a static context.
+     * 
+     * @test
+     */
+    public function testStaticSlugGeneratorWhenEntriesExist()
+    {
+        $post = Post::create(['title' => 'My Test Post']);
+
+        $this->assertEquals('my-test-post', $post->slug);
+        $this->assertEquals('my-test-post-1', Post::createSlug('My Test Post'));
+    }
 }
