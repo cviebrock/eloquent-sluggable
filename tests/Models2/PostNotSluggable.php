b05e97aa2927079d20be59d3570f8883cbfa271b
Colin Viebrock <colin@viebrock.ca>
2016-06-01 16:15:27 -0500

    case sens fix

---
+++ tests/Models2/PostNotSluggable.php
@@ -0,0 +1,43 @@
+<?php namespace Cviebrock\EloquentSluggable\Tests\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class PostNotSluggable
+ *
+ * A test model that doesn't use the Sluggable package.
+ */
+class PostNotSluggable extends Model
+{
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'posts';
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['title', 'subtitle'];
+
+    /**
+     * Convert the model to its string representation.
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->title;
+    }
+}
---
+++ tests/Models2/PostWithRelation.php
@@ -0,0 +1,34 @@
+<?php namespace Cviebrock\EloquentSluggable\Tests\Models;
+
+/**
+ * Class PostWithRelation
+ *
+ * A test model used for the relationship tests.
+ */
+class PostWithRelation extends Post
+{
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => ['author.name', 'title'],
+            ]
+        ];
+    }
+
+    /**
+     * Relation to Author model.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function author()
+    {
+        return $this->belongsTo(Author::class);
+    }
+}
